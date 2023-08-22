36e5676bd811c46d115a976ab52923c39ac342c4
saidt <saeid@metropolia.fi>
2023-08-22 15:35:12 +0300

    tehtävä 2

---
+++ tehtävä 5.py
@@ -0,0 +1,6 @@
+number1 = int(input("Nimeä yksi kokonaisluku."))
+number2 = int(input("Nimeä toinen kokonaisluku."))
+number3 = int(input("Nimeä kolmas kokonaisluku."))
+print("Kokonaislukujen summa on", number1+number2+number3)
+print("Kokonaislukujen tulo on", number1*number2*number3)
+print("Kokonaislukujen keskiarvo on", (number1+number2+number3)/3)
