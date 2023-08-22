d3cdaeefc1981e83d3a3d02e2cd617c4443b20c7
saidt <saeid@metropolia.fi>
2023-08-22 15:10:13 +0300

    tehtävä 2 part 4

---
+++ tehtävä 4.py
@@ -0,0 +1,4 @@
+width = int(input("Mikä on suorakolmion kannan pituus?"))
+height = int(input("Mikä on suorakolmion korkeus?"))
+print("Suorakulmion piiri on", width*2+height*2)
+print("suorakulmion pinta-ala", width*height)
