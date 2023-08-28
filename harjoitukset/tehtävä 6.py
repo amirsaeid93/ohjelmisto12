19b0d65df8e4a75c1eaa5e27a48f611750f7275c
saidt <saeid@metropolia.fi>
2023-08-28 11:29:34 +0300

    tehtävä 2

--- harjoitukset/tehtävä 6.py
+++ harjoitukset/tehtävä 6.py
@@ -3,18 +3,18 @@ leiviskät_nauloina = 20
 naulat_luoteina= 32
 luodit_grammoina = 13.3
 
-# Get user input for talents, pounds, and lots
+# kysymykset
 leiviskät = float(input("Anna leiviskät.\n"))
 naulat = float(input("Anna naulat. \n"))
 luodit = float(input("Anna luodit:\n"))
 
-# Calculate the total weight in lots
+# koko massa luoteina
 luodit_yhteensä = (leiviskät * leiviskät_nauloina * naulat_luoteina) + (naulat * naulat_luoteina) + luodit
 
-# Convert lots to kilograms and grams
-kilograms = luodit_yhteensä * luodit_grammoina / 1000
-grams = luodit_yhteensä * luodit_grammoina % 1000
+# muunnos luodeista kilogrammaan ja grammaan
+kilogramma = luodit_yhteensä * luodit_grammoina / 1000
+gramma = luodit_yhteensä * luodit_grammoina % 1000
 
-# Display the converted weight in modern units
+# Massa kilogrammana ja grammana
 print("\nMassa nykymittojen mukaan:")
-print(f"{int(kilograms)} kilogrammaa ja {grams:.2f} grammaa.")
+print(f"{int(kilogramma)} kilogrammaa ja {gramma:.2f} grammaa.")
--- tehtävät 2/tehtävä 7.py
+++ tehtävät 2/tehtävä 7.py
@@ -1,20 +1,15 @@
-print("Anna leiviskät.")
-leiv = input()
-print()
+import random
 
-print("Anna naulat.")
-naul = input()
-print()
+def generate_3_digit_combination():
+    return random.randint(0, 9), random.randint(0, 9), random.randint(0, 9)
 
-print("Anna luodit.")
-luo = input()
-print()
+def generate_4_digit_combination():
+    return random.randint(1, 6), random.randint(1, 6), random.randint(1, 6), random.randint(1, 6)
 
-kg = float(((leiv*20*32*13.3)+(naul*32*13.3)+(luo*13.3))//1000)
-g = float(((leiv*20*32*13.3)+(naul*32*13.3)+(luo*13.3))%1000)
+print("Random 3-digit combination:")
+combo_3_digit = generate_3_digit_combination()
+print(combo_3_digit)
 
-print("Massa nykymittojen mukaan:")
-print(f"{float(kg):2d} + kilogrammaa ja + {float(g):5.2f} + grammaa.")
-
-
-
+print("\nRandom 4-digit combination:")
+combo_4_digit = generate_4_digit_combination()
+print(combo_4_digit)
