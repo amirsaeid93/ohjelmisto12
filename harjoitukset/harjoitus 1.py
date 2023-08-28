2ccbb9bb027914fa757557a1cdc08bb4c378b187
saidt <saeid@metropolia.fi>
2023-08-28 16:11:50 +0300

    tehtävä 2

---
+++ harjoitukset/harjoitus 1.py
@@ -0,0 +1,21 @@
+rahaa = float(input("Anna rahan määrä:"))
+
+ehto = rahaa >= 5
+print(ehto)
+
+if rahaa >= 5:
+	print("saat latten.")
+
+	suutari = input("Anna suutarin nimi: ")
+	räätäli = input("Anna räätälin nimi: ")
+
+	if suutari == räätäli:
+		print("Hyvänen aika! Suutari ja räätäli ovat kaimoja!")
+
+	luku = float(input("Anna numero:"))
+	if luku > 0:
+		print("numero on positiivinen")
+	if luku == ("numero on nolla"):
+
+	if luku < 0:
+		print("numero on negatiivinen")
--- tehtävät 2/tehtävä 5.py
+++ tehtävät 2/tehtävä 5.py
@@ -1,6 +1,7 @@
+
 number1 = int(input("Nimeä yksi kokonaisluku."))
 number2 = int(input("Nimeä toinen kokonaisluku."))
 number3 = int(input("Nimeä kolmas kokonaisluku."))
 print("Kokonaislukujen summa on", number1+number2+number3)
 print("Kokonaislukujen tulo on", number1*number2*number3)
-print("Kokonaislukujen keskiarvo on", round((number1+number2+number3)/3))
+print("Kokonaislukujen keskiarvo on", (number1+number2+number3)/3)
