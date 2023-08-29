5efa13aed3ea44dcf81b60a239b93b734428c757
saidt <saeid@metropolia.fi>
2023-08-29 13:46:28 +0300

    tehtävä 2

---
+++ tehtävä 3/tehtävä 3.py
@@ -0,0 +1,18 @@
+sukupuoli = str(input("Mikä on sukupuolesi M vai N?"))
+hemoglobiini = float(input("Mikä on hemoglobiini arvosi?"))
+if sukupuoli == "M" and hemoglobiini < 134:
+    print("Hemoglobiiinisi on alhainen")
+elif sukupuoli == "M" and hemoglobiini >= 134 and hemoglobiini <= 195:
+    print("Hemoglobiinisi on normaali")
+
+elif sukupuoli == "M" and hemoglobiini > 195:
+    print("Hemoglobiinisi on korkeaa")
+
+elif sukupuoli == "N" and hemoglobiini < 117:
+    print("Hemoglobiinisi on alhainen.")
+
+elif sukupuoli == "N" and hemoglobiini >= 117 and hemoglobiini <= 175:
+    print("hemoglobiinisi on normaali.")
+
+elif sukupuoli == "N" and hemoglobiini > 175:
+    print("Hemoglobiinisi on korkeaa.")
