290b472b2350d9fcaeaa1cfdade0d9739f31827a
saidt <saeid@metropolia.fi>
2023-08-29 11:47:04 +0300

    tehtävä 2

--- tehtävä 3/tehtävä 1.py
+++ tehtävä 3/tehtävä 1.py
@@ -1,5 +1,5 @@
 kuha = float(input("Mikä on kalastamasi kuhan pituus senttimetreinä?"))
 if (kuha<37):
-    print("Päästä kuha takaisin mereen. Vain kuhat jotka ovat pituudeltaan suurempia tai yhtä suuria kuin 37 cm ovat tarpeeksi isoja syötäväksi.")
+    print("Päästä kuha takaisin mereen, koska kalastamasi kuha on ", (37-kuha), " cm liian lyhyt syötäväksi.")
 if (kuha>=37):
     print("Kuhasi on tarpeeksi iso syötäväksi.")
---
+++ tehtävä 3/tehtävä 2.py
@@ -0,0 +1,7 @@
+hytti = str(input("Mikä seuraavista hytti luokista LUX,A,B C on hyttisi luokka?"))
+
+if hytti == LUX:
+    print("Hyttisi on parvekkeellinen ja se on yläkannella.")
+
+if hytti == A
+    print("Hyttisi on ikkunallinen hytti autokannen yläpuolella.")
