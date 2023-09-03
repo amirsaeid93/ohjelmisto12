8e07808f1cc64b23b84fe081f6ecfef3c7b48957
saidt <saeid@metropolia.fi>
2023-09-03 23:57:15 +0300

    tehtävä 2

--- tehtävä 3/tehtävä 1.py
+++ tehtävä 3/tehtävä 1.py
@@ -1,5 +1,5 @@
 kuha = float(input("Mikä on kalastamasi kuhan pituus senttimetreinä?"))
 if (kuha<37):
-    print("Päästä kuha takaisin mereen, koska kalastamasi kuha on ", (37-kuha), " cm liian lyhyt syötäväksi.")
+    print("Päästä kuha takaisin mereen, koska kalastamasi kuha on", (37-kuha), "cm liian lyhyt syötäväksi.")
 if (kuha>=37):
-    print("Kuhasi on tarpeeksi iso syötäväksi.")
+    print("Kalastamasi kuha on tarpeeksi iso syötäväksi.")
