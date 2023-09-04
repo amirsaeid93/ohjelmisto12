70a308a827eaba88a84f1d61dbe5000eb59f04bc
saidt <saeid@metropolia.fi>
2023-09-04 14:37:06 +0300

    tehtävä 2

---
+++ harjoitukset/harjoitus 3'.py
@@ -0,0 +1,6 @@
+iterator = 0
+while iterator < 1000:
+    if iterator % 3 == 0:
+        print(iterator)
+    iterator += 1
+print("iterator arvo lopuksi: " + str(iterator))
--- tehtävä 3/tehtävä 4.py
+++ tehtävä 3/tehtävä 4.py
@@ -1,7 +1,10 @@
 
 vuosi = int(input("Syötä vuosi-numero:"))
 
-if (vuosi % 4 == 0 and vuosi % 100 != 0) or (vuosi % 400 == 0):
+if vuosi > 100 and vuosi % 4 == 0 and vuosi % 400 == 0:
+    print(vuosi, "on karkausvuosi.")
+elif vuosi < 100 and vuosi % 4 == 0:
     print(vuosi, "on karkausvuosi.")
 else:
     print(vuosi, "ei ole karkausvuosi.")
+
